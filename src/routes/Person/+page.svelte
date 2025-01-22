3b7486dd71bc7483ccc247b775b7005b9fc1342a
JaredQuicho <quichojaredjoenyl@gmail.com>
2025-01-22 10:59:51 +0800

    Success fetch from API

---
+++ src/routes/Person/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+
+    let {data} = $props()
+    console.log(data);
+</script>
+
+<div>
+    {#if data.datas.length > 0}
+        {#each data.datas as person (person.id)} 
+            <div>
+                <h1>{person.name}</h1>
+                <p>Language: {person.language}</p>
+                <p>ID: {person.id}</p>
+                <p>Bio: {person.bio}</p>
+                <p>Version: {person.version}</p>
+            </div>
+        {/each}
+    {:else}
+        <p>No data available</p>
+    {/if}
+</div>
